d526e22e735b7da4e3abb153ecdcd8f1d763ee37
Avi Flombaum <avi@flombaum.com>
2017-11-12 01:45:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance){
-  if (distance < 400){
+  if (distance <= 400){
     return 'This one is on me!'
+  } else if (distance > 2000){
+    return 'I will gladly take your thirty bucks.'
   }
 }
